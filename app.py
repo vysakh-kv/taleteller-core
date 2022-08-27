41c354eb27dcebed960b6fa3b28ca62fce0f7c51
Vysakh T <vysakh.t@keyvalue.systems>
2022-08-27 12:09:41 +0530

    adding merge api

--- app.py
+++ app.py
@@ -40,3 +40,62 @@ def tts():
             f"{id}.mp3", 
             mimetype="audio/mpeg", 
             as_attachment=True)
+
+
+@app.route("/merge", methods=['POST'])
+def merge():​
+# Request ID Logic here
+    id = 'id'
+    if request.method=='POST':
+    images = request.files.getlist("images")
+    audios = request.files.getlist("audio")
+
+    N = len(images)
+    clips = []
+    print(images)
+    print(audios)
+    for i in range(N):
+
+    f = open(f"{images[i].filename}","wb")
+    images[i].save(f)
+    f.close()
+    f = open(f"{audios[i].filename}","wb")
+    audios[i].save(f)
+    f.close()
+
+    add_static_image_to_audio(images[i].filename,audios[i].filename,f"{id}_{images[i].filename}.mp4")
+    clips.append(f"{id}_{images[i].filename}.mp4")
+    concatenate(clips,id)
+    return send_file(
+    f"{id}_merged.mp4", 
+    mimetype="video/mp4", 
+    as_attachment=True)
+
+
+@app.route("/merge/b64", methods=['POST'])
+def mergeb64():
+
+    # Request ID Logic here
+    id = 'id'
+    if request.method=='POST':
+        images = request.json["images"]
+        audios = request.json["audio"]
+        afmt = request.json["audiofmt"]
+        ifmt = request.json["imgfmt"]
+
+        N = len(images)
+        clips = []
+        for i in range(N):
+            f = open(f"{id}_{i}.{ifmt}", "wb")
+            f.write(base64.b64decode(images[i]))
+            f.close()
+            f = open(f"{id}_{i}.{afmt}", "wb")
+            f.write(base64.b64decode(audios[i]))
+            f.close()
+            add_static_image_to_audio(f"{id}_{i}.{ifmt}",f"{id}_{i}.{afmt}",f"{id}_{i}.mp4")
+            clips.append(f"{id}_{i}.mp4")
+        concatenate(clips,id)
+        return send_file(
+            f"{id}_merged.mp4", 
+            mimetype="video/mp4", 
+            as_attachment=True)
